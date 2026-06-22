e954820990c5285923f8869383a2a471ef063238
ryan <ryanhuang8989@gmail.com>
2026-06-22 01:13:18 -0400

    Add lab files

---
+++ lab1_intro/packet_data.sv
@@ -0,0 +1,25 @@
+/*-----------------------------------------------------------------
+File name     : packet_data.sv
+Developers    : Brian Dickinson
+Created       : 01/08/19
+Description   : lab1 packet data item 
+Notes         : From the Cadence "Essential SystemVerilog for UVM" training
+-------------------------------------------------------------------
+Copyright Cadence Design Systems (c)2019
+-----------------------------------------------------------------*/
+
+// Follow instructions in lab book
+  
+// add print and type policies here
+
+// packet class
+class packet;
+
+  // add properties here
+
+  // add constructor to set instance name and source by arguments and packet type
+
+ // add print with policy
+ 
+endclass
+
