4bea72233c125b8809a798dcfdbb716154af365e
ryan <ryanhuang8989@gmail.com>
2026-06-25 00:19:30 -0400

    lab 1 steps 1a to 1c

--- lab1_intro/packet_data.sv
+++ lab1_intro/packet_data.sv
@@ -11,14 +11,23 @@ Copyright Cadence Design Systems (c)2019
 // Follow instructions in lab book
   
 // add print and type policies here
-
+typedef enum {ANY, SINGLE, MULTICAST, BROADCAST} packet_type_t;
 // packet class
 class packet;
 
   // add properties here
+  local string name;
+  bit[3:0] target;
+  bit[3:0] source;
+  bit[7:0] data;
+  packet_type_t ptype;
 
   // add constructor to set instance name and source by arguments and packet type
-
+  function new (string name, int port)
+    this.name = name;
+    this.source = 4'b0001 << port;
+    this.ptype = ANY;
+  endfunction
  // add print with policy
  
 endclass
